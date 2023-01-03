ef2a2aac1a234f55b2309b793deb0b4d2b8a8c8d
Tadesse Jemal <tadessejemal63@gmail.com>
2023-01-03 22:30:00 +0300

    add controller

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,3 +1,11 @@
-class Api::V1::GreetingsController < ApplicationController
-  def index; end
+class MessagesController < ApplicationController
+  def index
+    id = rand(1..Message.count)
+    if id.nil?
+      render json: { error: 'not found' }.to_json, status: 404
+    else
+      @message = Message.find(id)
+      render json: @message
+    end
+  end
 end
